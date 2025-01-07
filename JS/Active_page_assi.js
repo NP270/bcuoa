c55f1f10ca5e81d508028cadc9c150f8d42bcde0
NP270 <nethularandes29@gmail.com>
2025-01-07 07:56:05 +0530

    Contact update

--- JS/Active_page_assi.js
+++ JS/Active_page_assi.js
@@ -1,9 +1,7 @@
 const navLinks = document.querySelectorAll('.navbar a');
-const currentPage = window.location.pathname.replace(/\/$/, '').toLowerCase();
+const currentPage = window.location.pathname;
 navLinks.forEach(link => {
-  if (link.dataset.page === 'home' && (currentPage === '' || currentPage === '/index.html')) {
-    link.classList.add('active');
-  } else if (link.href.includes(currentPage)) {
+  if (link.href.includes(currentPage)) {
     link.classList.add('active');
   }
 });
